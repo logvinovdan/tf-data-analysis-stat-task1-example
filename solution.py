c36d22f5dfcac78632db9d4fcd4d47b552cd0985
logvinovdan <76569554+logvinovdan@users.noreply.github.com>
2023-04-08 02:49:51 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,17 +1,9 @@
 import pandas as pd
 import numpy as np
-import math
+
 
 chat_id = 834639322 # Ваш chat ID, не меняйте название переменной
 
-def solution(x: np.array) -> float:
-    mu = -31 + math.exp(1)
-    size = len(x)
-    v = 0
-    t1 = 0
-    t = 10
-    for i in range(size):
-        v += x[i]*(t-mu)
-        t1 += (t - mu)**2
-    a = v/t1
-    return a
+def solution(x: np.array) -> float:   # v = v_0 + at => a = v/10
+    a = x.mean()/ 10
+    return a # Ваш ответ
