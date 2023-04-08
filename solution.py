7e256f28d90e0f764877a0f20ffb1a619035b42a
logvinovdan <76569554+logvinovdan@users.noreply.github.com>
2023-04-08 03:24:44 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import numpy as np
+from scipy.optimize import minimize
+from scipy.stats import lognorm
 
 chat_id = 834639322 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    x = ( x - (-31 + np.exp(1))) / 10
-    return x.mean() # Ваш ответ
+    x -= 31
+    result = np.mean(np.log(x))
+    return result # Ваш ответ
