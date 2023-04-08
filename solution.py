b7454bae7d1bcc7baa6a200b3a6d9af712ff292a
logvinovdan <76569554+logvinovdan@users.noreply.github.com>
2023-04-08 03:15:16 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,13 +1,8 @@
 import pandas as pd
 import numpy as np
 
-
-chat_id = 834639322
+chat_id = 834639322 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    n = len(x)
-    time = 10
-    errors = np.random.exponential(scale=1, size=n) - 31
-    v_real = x - errors
-    a = np.mean(v_real) / time
-    return a
+    x = ( x - (-31 + np.exp(1))) / 10
+    return x.mean() # Ваш ответ
